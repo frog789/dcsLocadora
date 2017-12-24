64215f06b03e422529c637cc034c802b35a08945
Raphael Almeida <raph.tads2013@gmail.com>
2017-12-24 16:31:58 -0300

    Ajustes Entidades

--- src/main/java/com/dsclocadora/entities/Jogo.java
+++ src/main/java/com/dsclocadora/entities/Jogo.java
@@ -32,164 +32,215 @@ import org.hibernate.validator.constraints.br.CPF;
 import com.dsclocadora.entities.Categoria;
 
 @Entity
-@Table (name = "TB_Jogo")
-public class Jogo implements Serializable{
-	
-	private static final long serialVersionUID = 1L;
+@Table(name = "TB_Jogo")
+public class Jogo implements Serializable {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    private static final long serialVersionUID = 1L;
 
-	@NotBlank
-	@Column(name = "titulo", length = 60, nullable = false)
-	private String titulo;
-	
-	@NotBlank
-	@Column(name = "quantidade_disponivel", length = 60, nullable = false)
-	private int qtdDisponivel;
-	
-	@NotBlank
-	@Column(name = "quantidade_jogadores", length = 60, nullable = false)
-	private int qtdJogadores;
-	
-	@NotBlank
-	@Column(name = "tam_jogo", length = 60, nullable = false)
-	private Double tamanhoGB;
-	
-	@NotBlank
-	@Column(name = "classificacao_etaria", length = 60, nullable = false)
-	private FaixaEtariaEnum classificacao;
-	
-	@NotBlank
-	@Column(name = "audio_idioma", length = 60, nullable = false)
-	private Idioma audioIdioma;
-	
-	@NotBlank
-	@Column(name = "tam_jogo", length = 60, nullable = false)
-	private Idioma legendaIdioma;
-	
-	@NotBlank
-	@Column(name = "nome_criador", length = 60, nullable = false)
-	private String criador;
-	
-	@NotBlank
-	@Column(name = "produtora", length = 60, nullable = false)
-	private String produtora;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@NotNull
-	@Column(name = "dt_lancamento", length = 32, nullable = false)
-	protected Date dtLancamento;
-	
-	@Enumerated (EnumType.STRING)
-	private FaixaEtariaEnum pegRating;
-	
-	@ManyToMany (fetch = FetchType.LAZY)
-	@JoinTable(name = "games_categoria", joinColumns = {
-			@JoinColumn(name = "ID_GAME")},
-			inverseJoinColumns = {
-					@JoinColumn(name = "ID_CATEGORIA")})
-	private List<Jogo> categorias;
+    @NotBlank
+    @Column(name = "titulo", length = 60, nullable = false)
+    private String titulo;
 
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "data_aluguel")
-	protected Date dtAluguel;	
-	
-	public Long getId() {
-		return id;
-	}
+    @NotBlank
+    @Column(name = "quantidade_disponivel", length = 60, nullable = false)
+    private int qtdDisponivel;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    @NotBlank
+    @Column(name = "quantidade_jogadores", length = 60, nullable = false)
+    private int qtdJogadores;
 
-	public String getTitulo() {
-		return titulo;
-	}
+    @NotBlank
+    @Column(name = "tam_jogo", length = 60, nullable = false)
+    private Double tamanhoGB;
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
+    @NotBlank
+    @Enumerated(EnumType.STRING)
+    private FaixaEtariaEnum classificacao;
 
-	public String getCriador() {
-		return criador;
-	}
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "games_audio", joinColumns = {
+        @JoinColumn(name = "ID_GAME")},
+            inverseJoinColumns = {
+                @JoinColumn(name = "ID_AUDIO")})
+    private Idioma audioIdioma;
 
-	public void setCriador(String criador) {
-		this.criador = criador;
-	}
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "games_legenda", joinColumns = {
+        @JoinColumn(name = "ID_GAME")},
+            inverseJoinColumns = {
+                @JoinColumn(name = "ID_LEGENDA")})
+    private Idioma legendaIdioma;
 
-	public Double getTamanhoGB() {
-		return tamanhoGB;
-	}
+    @NotBlank
+    @Column(name = "nome_criador", length = 60, nullable = false)
+    private String nomeCriador;
 
-	public void setTamanhoGB(Double tamanhoGB) {
-		this.tamanhoGB = tamanhoGB;
-	}
+    @NotBlank
+    @Column(name = "produtora", length = 60, nullable = false)
+    private Produtora produtora;
 
-	public String getProdutora() {
-		return produtora;
-	}
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "games_categoria", joinColumns = {
+        @JoinColumn(name = "ID_GAME")},
+            inverseJoinColumns = {
+                @JoinColumn(name = "ID_CATEGORIA")})
+    private List<Jogo> categorias;
 
-	public void setProdutora(String produtora) {
-		this.produtora = produtora;
-	}
+    @Temporal(TemporalType.DATE)
+    @Column(name = "data_lancamento")
+    private Date dtLancamento;
 
-	public Date getDtLancamento() {
-		return dtLancamento;
-	}
+    @NotBlank
+    @Column(name = "valor_jogo", length = 60, nullable = false)
+    private Double valorJogo;
 
-	public void setDtLancamento(Date dtLancamento) {
-		this.dtLancamento = dtLancamento;
-	}
+    @NotBlank
+    @Column(name = "valor_locacao", length = 60, nullable = false)
+    private Double valorLocacao;
 
-	public FaixaEtariaEnum getPegRating() {
-		return pegRating;
-	}
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id){
+        this.id = id;
+    }
 
-	public void setPegRating(FaixaEtariaEnum pegRating) {
-		this.pegRating = pegRating;
-	}
+    public String getTitulo() {
+        return titulo;
+    }
 
-	public List<Jogo> getCategorias() {
-		return categorias;
-	}
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
-	public void setCategorias(List<Jogo> categorias) {
-		this.categorias = categorias;
-	}
+    public int getQtdDisponivel() {
+        return qtdDisponivel;
+    }
 
-	public Date getDtAluguel() {
-		return dtAluguel;
-	}
+    public void setQtdDisponivel(int qtdDisponivel) {
+        this.qtdDisponivel = qtdDisponivel;
+    }
 
-	public void setDtAluguel(Date dtAluguel) {
-		this.dtAluguel = dtAluguel;
-	}
+    public int getQtdJogadores() {
+        return qtdJogadores;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+    public void setQtdJogadores(int qtdJogadores) {
+        this.qtdJogadores = qtdJogadores;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (!(obj instanceof Jogo))
-			return false;
-		Jogo other = (Jogo) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+    public Double getTamanhoGB() {
+        return tamanhoGB;
+    }
 
+    public void setTamanhoGB(Double tamanhoGB) {
+        this.tamanhoGB = tamanhoGB;
+    }
+
+    public FaixaEtariaEnum getClassificacao() {
+        return classificacao;
+    }
+
+    public void setClassificacao(FaixaEtariaEnum classificacao) {
+        this.classificacao = classificacao;
+    }
+
+    public Idioma getAudioIdioma() {
+        return audioIdioma;
+    }
+
+    public void setAudioIdioma(Idioma audioIdioma) {
+        this.audioIdioma = audioIdioma;
+    }
+
+    public Idioma getLegendaIdioma() {
+        return legendaIdioma;
+    }
+
+    public void setLegendaIdioma(Idioma legendaIdioma) {
+        this.legendaIdioma = legendaIdioma;
+    }
+
+    public String getCriador() {
+        return nomeCriador;
+    }
+
+    public void setCriador(String criador) {
+        this.nomeCriador = criador;
+    }
+
+    public Produtora getProdutora() {
+        return produtora;
+    }
+
+    public void setProdutora(Produtora produtora) {
+        this.produtora = produtora;
+    }
+
+    public List<Jogo> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<Jogo> categorias) {
+        this.categorias = categorias;
+    }
+
+    public Date getDtLancamento() {
+        return dtLancamento;
+    }
+
+    public void setDtLancamento(Date dtLancamento) {
+        this.dtLancamento = dtLancamento;
+    }
+
+    public Double getValorJogo() {
+        return valorJogo;
+    }
+
+    public void setValorJogo(Double valorJogo) {
+        this.valorJogo = valorJogo;
+    }
+
+    public Double getValorLocacao() {
+        return valorLocacao;
+    }
+
+    public void setValorLocacao(Double valorLocacao) {
+        this.valorLocacao = valorLocacao;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Jogo)) {
+            return false;
+        }
+        Jogo other = (Jogo) obj;
+        if (getId() == null) {
+            if (other.getId() != null) {
+                return false;
+            }
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        return true;
+    }
 }
